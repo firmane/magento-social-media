e1c29d6a6ef2a5f48c20ea3629d1976dc16d2f0a
firmane <firman.elance@yahoo.com>
2014-04-24 05:20:57 +0700

    cron observer

---
+++ app/code/community/Tebet/SocialMedia/Model/Resource/Share/Collection.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Magento - Social Media
+ *
+ * Engage the most influential audiences on the most social content
+ * and see superior results.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Tebet
+ * @package    Tebet_SocialMedia
+ * @copyright  Copyright (c) 2013 Firman Efendi <firman.elance@yahoo.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Tebet_SocialMedia_Model_Resource_Share_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
+    public function addUpdateRequiredFilter()
+    {
+    }
+}
---
+++ app/code/community/Tebet/SocialMedia/Model/Share.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Magento - Social Media
+ *
+ * Engage the most influential audiences on the most social content
+ * and see superior results.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Tebet
+ * @package    Tebet_SocialMedia
+ * @copyright  Copyright (c) 2013 Firman Efendi <firman.elance@yahoo.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Tebet_SocialMedia_Model_Share extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('socialmedia/share');
+    }
+}
